494bf313143a00af0d85d2369b5203c3289cc749
Ai-cloud-rain <91471536+Ai-cloud-rain@users.noreply.github.com>
2024-10-15 20:02:16 +0900

    プロジェクトをセットアップ

---
+++ src/test/java/raisetech_2/studentmanagement_2/Studentmanagement2ApplicationTests.java
@@ -0,0 +1,13 @@
+package raisetech_2.studentmanagement_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Studentmanagement2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
